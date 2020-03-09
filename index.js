4da200f3016cb1d966975610fdba3cdb32bf35e5
WirteR <kravetsillis16@gmail.com>
2020-03-09 09:33:23 +0200

    call-me added

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+
+$(document).ready(function()
 {
-    var elem = document.querySelector('.phone');
+    var elem = $(".phone"); 
+    elem.click(function(e) {
+        if (!$(e.target).hasClass("call-me__active") && e.target.id !== "cancel-widget") {
+            if (!$(this).html()) {
+                $(this).addClass("call-me__active");
+                $(this).append( '<div id="call-section"><div><div id="cancel-widget"></div></div> \
+                                <form> \
+                                    <p>Передзвонимо Вам найближчим часом</p> \
+                                    <input type="text" placeholder="Ваш номер телефону"></input> \
+                                    <input type="submit" value="Чекаю дзвінка"></input> \
+                                </form></div>');
+                $("#call-section").hide();
+                $(".call-me__active").bind('transitionend', function() {
+                    $("#call-section").fadeIn(150);        
+                });
+            }
+        }
+    });
     
-    elem.addEventListener('click', function () {
-        this.classList.toggle('call-me__active');
-            this.insertAdjacentHTML('afterbegin', 
-                '<form> \
-                    <p>Передзвонимо Вам найближчим часом</p> \
-                    <input type="text" placeholder="Ваш номер телефону"></input> \
-                    <input type="submit" value="Чекаю дзвінка"></input> \
-                </form>'
-            );      
-    })
-}
+    elem.on('click', '#cancel-widget', function() {
+        elem.removeClass('call-me__active');  
+        elem.empty();
+    });
+    
+});
